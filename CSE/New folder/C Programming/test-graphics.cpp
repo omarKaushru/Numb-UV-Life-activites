e9ee1e57ced480e77e933d3415436cd4c5e8bc97
Md. Omar Kaushru <omarkaushru@gmail.com>
2020-09-02 00:14:24 +0600

    Little chnages in directory

--- CSE/New folder/C Programming/test-graphics.cpp
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-#include<graphics.h>
-
-int main()
-{
-       initwindow(400, 300, "First Sample");
-    circle(50,50,50);
-getch();
-}
--- CSE/New folder/Sattar sir/C Programming/test-graphics.cpp
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-#include<graphics.h>
-
-int main()
-{
-       initwindow(400, 300, "First Sample");
-    circle(150,150,50);
-getch();
-}
